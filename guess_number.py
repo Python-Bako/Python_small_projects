317943db52604b576fc97eb3609ef4bd128205b3
Nikos Bako <bako@zhaw.ch>
2021-04-27 14:47:45 +0200

    small script aded

---
+++ guess_number.py
@@ -0,0 +1,19 @@
+
+# small script for guessing a random number
+
+import random
+
+def guess(x):
+    random_num = random.randint(1,x)
+    guess = 0
+
+    while guess != random_num:
+        guess = int(input(f"Enter a random number between 1 and {x}: "))
+        if guess > random_num:
+            print("Sorry, guess again. Too high!")
+        elif guess < random_num:
+            print("Sorry, guess again. Too low!")
+        # there is no need to add an else statement since if the guess == random_num then we are exiting the while loop
+    print(f"Congats you have guessed the number!!!!!!")
+
+guess(10)
